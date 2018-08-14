7d2c9cc20d1744665c92b152fd7f33e5d7eb655f
Simon Vandekar <vandeks@Simons-MacBook-Pro.local>
2018-08-14 12:18:44 -0500

    Updated argument names. Introduced formula syntax in computeStats.

--- R/getDesign.R
+++ R/getDesign.R
@@ -2,25 +2,25 @@
 #'
 #' This function gets a design matrix for a generalized additive model (GAM)
 #'  from the mgcv package or a linear model.
-#' @param eq A formula or string beginning with ~ that can be interpreted as an mgcv or lm
+#' @param form A formula or string beginning with ~ that can be interpreted as an mgcv or lm
 #'  formula without the outcome variable.
-#' @param data A data frame containing the variables specified in eq.
+#' @param data A data frame containing the variables specified in form.
 #' @keywords design matrix
-#' @return Returns a design matrix constructed from eq and data.
+#' @return Returns a design matrix constructed from form and data.
 #' @importFrom mgcv gam
 #' @importFrom stats as.formula model.matrix update.formula
 #' @export
-getDesign = function(eq, data){
+getDesign = function(form, data){
   # if it looks like a gam formula
-  if(any(grepl("s\\(", eq))){
+  if(any(grepl("s\\(", form))){
     # 1:n because you need to give gam an outcome to easily get the design matrix
     data$x = 1:nrow(data)
-    lmfull = if(class(eq)=='formula') update.formula(eq, x ~ .) else paste('x', eq)
+    lmfull = if(class(form)=='formula') update.formula(form, x ~ .) else paste('x', form)
     lmfull = model.matrix(mgcv::gam(as.formula(lmfull), data=data) )
 
   # else it's a linear model
   } else {
-    lmfull = model.matrix(as.formula(eq), data=data)
+    lmfull = model.matrix(as.formula(form), data=data)
   }
   return(lmfull)
 }
