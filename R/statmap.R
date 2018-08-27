bea3f26713cbf4313ed22dc10af32d13bda55954
Simon Vandekar <simon.vandekar@vanderbilt.edu>
2018-08-27 17:21:48 -0500

    first parts of visualization.

--- R/statmap.R
+++ R/statmap.R
@@ -56,3 +56,46 @@ plot.statMap <- function(x, slice=1, ...)
 {
   stop("FILL IN PLOTTING FUNCTION HERE. SLICE IS an example of an additional parameter")
 }
+
+# modified from oro.nifti:::image.nifti
+image.statMap = function (statmap, col = gray(0:64/64), plane = c("axial", 
+    "coronal", "sagittal"), xlab = "", ylab = "", axes = FALSE, oma = rep(0, 
+        4), mar = rep(0, 4), bg = "black", ...) 
+{
+    x = if(is.character(statmap$template)) readNifti(statmap$template) else statmap$template
+    pixdim = RNifti::pixdim(x)
+    mask = if(is.character(statmap$mask)) readNifti(statmap$mask) else statmap$mask
+
+    switch(plane[1], axial = {
+        aspect <- pixdim[3]/pixdim[2]
+    }, coronal = {
+            x <- aperm(x, c(1, 3, 2))
+            mask <- aperm(mask, c(1, 3, 2))
+        aspect <- pixdim[4]/pixdim[2]
+    }, sagittal = {
+            x <- aperm(x, c(2, 3, 1))
+            mask <- aperm(mask, c(2, 3, 1))
+        aspect <- pixdim[4]/pixdim[3]
+    }, stop(paste("Orthogonal plane", plane[1], "is not valid.")))
+    # permuted image dimensions
+
+    # crop image and get image dimensions
+    xinds = apply(mask, 1, any)
+    yinds = apply(mask, 2, any)
+    zinds = apply(mask, 3, any)
+    x = x[xinds,,]
+    x = x[,yinds,]
+    x = x[,,zinds]
+    imgdim = dim(x)
+    index = 
+    lz <- length(index)
+    oldpar <- par(no.readonly = TRUE)
+    par(mfrow = ceiling(rep(sqrt(lz), 2)), oma = oma, mar = mar, bg = bg)
+    for (z in index) {
+        graphics::image(1:imgdim[1], 1:imgdim[2], x[, , z], col = col, 
+          breaks = breaks, asp = aspect, axes = axes, 
+          xlab = xlab, ylab = ylab, ...)
+    }
+    par(oldpar)
+    invisible()
+}
