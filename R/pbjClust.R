7b03b972d9a12b7d87f68c0d7816ddc74d56f262
Simon Vandekar <simon.vandekar@vanderbilt.edu>
2018-10-18 11:19:08 -0500

    removed dependence on bigmemory.

--- R/pbjClust.R
+++ R/pbjClust.R
@@ -19,9 +19,7 @@
 #' @importFrom utils setTxtProgressBar txtProgressBar
 #' @importFrom RNifti writeNifti updateNifti
 #' @importFrom mmand shapeKernel
-#' @import bigmemory
 #' @importFrom parallel mcparallel mccollect
-#' @import bigalgebra
 pbjClust = function(statMap, cfts=c(0.01, 0.005), nboot=5000, kernel='box'){
   if(class(statMap)[1] != 'statMap')
     warning('Class of first argument is not \'statMap\'.')
