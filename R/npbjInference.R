a402372ca7d6ab736d3b4e0c9159ca932d02a59a
Simon Vandekar <simon.vandekar@vanderbilt.edu>
2018-12-10 08:08:45 -0600

    changed cfts to be chi-square.

--- R/npbjInference.R
+++ R/npbjInference.R
@@ -9,6 +9,6 @@
 sei = function(stat, mask, cfts=c(0.01, 0.005), df=1, kernel='box'){
   k = mmand::shapeKernel(3, 3, type=kernel)
   tmp = mask
-  tmp = lapply(cfts, function(th){ tmp[ mask==1] = (stat>th); tmp})
+  tmp = lapply(qchisq(1-cfts, df), function(th){ tmp[ mask==1] = (stat>th); tmp})
   return(sapply(tmp, function(tm) max(c(table(c(mmand::components(tm, k))),0), na.rm=TRUE)))
 }
