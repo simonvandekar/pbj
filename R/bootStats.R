010cb27ae6b2e3f6c0dc86bc505187384c6a1f40
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-01-17 15:30:51 -0600

    Added type theory comments to bootStats and dependency computation reordered

--- R/bootStats.R
+++ R/bootStats.R
@@ -13,42 +13,33 @@
 #' @param statistic A function of the form statistic(statimg, mask) that returns a statistic of interest.
 #' @keywords statistical parametric map, nonparametric bootstrap
 #' @param ... Arguments passed to statistic function.
-#' @return Returns a list with the fo:
-#' \describe{
-#'   \item{stat}{The statistical nifti object. If ncol(X) = ncol(Xred)+1, then this is a Z-statistic map, otherwise it is a chi^2-statistic map.}
-#'   \item{sqrtSigma}{The 4d covariance object. This is a V by n matrix R, such that R \%*\% t(R) = Sigma.}
-#'   \item{mask}{The input mask.}
-#'   \item{template}{The background template used for visualization.}
-#'   \item{formulas}{A list containing the full and reduced models.}
-#'   \item{robust}{A logical indicating the input setting.}
-#'   \item{df}{The numerator degrees of freedom of the test.}
-#'   \item{rdf}{The numerator degrees of freedom of the test.}
-#' }
+#' @return Returns a vector of the resulting estimate
 #' @importFrom stats coefficients lm pf pt qnorm qchisq residuals
 #' @importFrom RNifti writeNifti readNifti
 #' @importFrom parallel mclapply
 #' @export
-bootStats = function(images, coefficients=0, X, Xred, statistic=function(stat) stat, ...){
+bootStats = function(images,                            # M(n, i)
+                     coefficients=0,                    # Maybe M(k-r, i)
+                     X,                                 # M(n, k)
+                     Xred,                              # M(n, r)
+                     statistic=function(stat, ...) stat,# V(i) -> argv -> V(i)
+                     ...)                               # argv
+{
+  rdf    <- nrow(X) - ncol(X)                           # UInt    // n - k
 
-  n     <- nrow(images)
-  peind <- which(!colnames(X) %in% colnames(Xred))
-  df    <- length(peind)
-  rdf   <- n - ncol(X)
+  QR     <- qr(X)                                       # M(n, k)
 
-  # fit model to all image data
-  # p X V
-  QR     <- qr(X)
-  bcoefs <- qr.coef(QR, images)[peind,] - coefficients
+  # compute numerator of inverse covariance of beta hat
+  peind  <- which(!colnames(X) %in% colnames(Xred))     # V(k-r)
+  bcoefs <- qr.coef(QR, images)[peind,] - coefficients  # M(k-r, i)
+  varX1  <- qr.resid(qr(Xred), X[,peind])               # M(n, k-r)
+  varX1  <- t(varX1) %*% varX1                          # M(k-r, k-r)
+  stat   <- colSums(bcoefs * (varX1 %*% bcoefs))        # V(i)
 
-  # compute the part of the inverse covariance of beta hat
-  varX1  <- qr.resid(qr(Xred), X[,peind])
-  varX1  <- t(varX1) %*% varX1
-  images <- qr.resid(QR, images)
-
-  # overwrite images with the other part of the inverse covariance of beta hat
-  images <- colSums(images^2)/rdf
-  stat   <- colSums(bcoefs * (varX1 %*% bcoefs))
+  # compute denominator of inverse covariance of beta hat
+  images <- qr.resid(QR, images)                        # M(n, i)
+  images <- colSums(images^2)/rdf                       # V(i)
 
   # This is the statistic
-  statistic(stat/images, ...)
+  statistic(stat/images, ...)                           # V(i)
 }
