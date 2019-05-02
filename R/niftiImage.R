16e1ae6c0a14726f6f660c7089b7ea13a1c58659
Simon Vandekar <simon.vandekar@vanderbilt.edu>
2019-05-02 16:50:27 -0500

    Added an image.niftiImage function.

--- R/niftiImage.R
+++ R/niftiImage.R
@@ -74,7 +74,8 @@ image.niftiImage = function (x, bgimg=NULL, thresh=0, index = NULL, col = gray(0
   breaksneg <- c(thresh, seq(thresh, maxstatneg, length = length(colneg)-1), maxstatneg)
   if(is.null(index)) index = 1:imgdim[3]
   oldpar <- par(no.readonly = TRUE)
-  par(mfrow = ceiling(rep(sqrt(length(index)), 2)), oma = oma, mar = mar, bg = bg)
+  if(length(index)!=1)
+    par(mfrow = ceiling(rep(sqrt(length(index)), 2)), oma = oma, mar = mar, bg = bg)
   for (z in index) {
     # background image
     graphics::image(1:imgdim[1], 1:imgdim[2], x[, , z], col = col,
