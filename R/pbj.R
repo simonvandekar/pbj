6069315a4cc41ec728336255a0b1b8bff0f25e11
Simon Vandekar <vandeks@ollie.local>
2020-04-10 13:24:08 -0500

    4 moment edgeworth expansion.

--- R/pbj.R
+++ R/pbj.R
@@ -144,5 +144,5 @@ image.CoPE <- function(x, alpha=0.05, ...)
 #' @param stat Vector of test statistics
 #' @param skew The third cumulant of the test statistics
 #' @importFrom PDQutils papx_edgeworth
-vpapx_edgeworth = Vectorize(function (stat, skew) PDQutils::papx_edgeworth(stat, raw.cumulants=c(0,1,skew) ) )
+vpapx_edgeworth = Vectorize(function (stat, mu3, mu4) PDQutils::papx_edgeworth(stat, raw.cumulants=moment2cumulant(c(0,1, mu3, mu4) ) ))
 
