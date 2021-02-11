515847236d0fe70d7ebb3de80225af8fae9e3637
Simon Vandekar <vandeks@ollie.local>
2021-02-11 15:20:04 -0600

    Added independence assumption version.

--- R/pbjBoot.R
+++ R/pbjBoot.R
@@ -12,7 +12,7 @@
 #' @return Returns vector of test statistics computed from the bootstrapped sample.
 #' @export
 #
-pbjBoot = function(sqrtSigma, rboot, bootdim, method=c('nonparametric', 't', 'conditional', 'permutation'), voxelwise=FALSE, HC3=TRUE, robust=TRUE, transform=c('none', 't')){
+pbjBoot = function(sqrtSigma, rboot, bootdim, method=c('t', 'independence', 'permutation', 'conditional', 'nonparametric'), voxelwise=FALSE, HC3=TRUE, robust=TRUE, transform=c('none', 't')){
   method = tolower(method[1])
   eps=0.001
   V = ncol(sqrtSigma$res)
@@ -29,25 +29,24 @@ pbjBoot = function(sqrtSigma, rboot, bootdim, method=c('nonparametric', 't', 'co
     }
     if(robust){
       if(method == 'conditional'){
-        if( length(bootdim)==0 ){ # dimension of bootstrap must be a vector of length n
-          boot = rboot(n)
-          # could be made faster b/c BsqrtInv does not need to be computed in each bootstrap (though it is here)
-          BsqrtInv = matrix(apply(sweep(simplify2array(rep(list(sweep(qr.resid(sqrtSigma$QR, sqrtSigma$res), 1, 1-h, '/')), df)), c(1,3), sqrtSigma$X1res, '*'), 2,
-                                  function(x){ backsolve(r=qr.R(qr(x)), x=diag(ncol(x))) }), nrow=df^2, ncol=V)
-          sqrtSigma$res = sweep(sqrtSigma$res, 1, boot/sqrt(1-h), '*')
-          statimg = simplify2array( lapply(1:V, function(ind) crossprod(matrix(BsqrtInv[,ind], nrow=df, ncol=df), crossprod(sqrtSigma$X1res, sqrtSigma$res[,ind]) ) ), higher=TRUE )
-        } else {
-          stop('Dimension of bootstrap sample is not correct for the method.')
-        }
+        boot = rboot(n)
+        # could be made faster b/c BsqrtInv does not need to be computed in each bootstrap (though it is here)
+        BsqrtInv = matrix(apply(sweep(simplify2array(rep(list(sweep(qr.resid(sqrtSigma$QR, sqrtSigma$res), 1, 1-h, '/')), df)), c(1,3), sqrtSigma$X1res, '*'), 2,
+                                function(x){ backsolve(r=qr.R(qr(x)), x=diag(ncol(x))) }), nrow=df^2, ncol=V)
+        sqrtSigma$res = sweep(sqrtSigma$res, 1, boot/sqrt(1-h), '*')
+        statimg = matrix(simplify2array( lapply(1:V, function(ind) crossprod(matrix(BsqrtInv[,ind], nrow=df, ncol=df), crossprod(sqrtSigma$X1res, sqrtSigma$res[,ind]) ) ), higher=TRUE ), nrow=df, ncol=V)
+      } else if(method=='independence'){
+        # n x df array
+        boot = replicate(df, rboot(n))
+        # Divide by 1-h for debiased residuals. result is n x V x df array
+        statimg = sweep(simplify2array(rep(list(sweep(sqrtSigma$res, 1, 1-h, '/')), df), higher = TRUE), c(1,3), sqrtSigma$X1res * boot, '*')
+        # scale to unit variance and compute standardized parameter estimates.
+        statimg = matrix(colMeans(apply(statimg, c(2,3), scale), dims = 2), nrow=df, ncol=V)
       } else{
         if(method=='t'){#is.list(sqrtSigma)){ sqrtSigma should be a list here
-          if( length(bootdim)==0 ){ # dimension of bootstrap must be a vector of length n
-            sqrtSigma$res = sweep(sqrtSigma$res, 1, rboot(n)/sqrt(1-h), '*')
-            #sigmas = sqrt(colSums(qr.resid(sqrtSigma$QR, sqrtSigma$res)^2)/rdf)
-            #sqrtSigma$res = sweep(sqrtSigma$res, 2, sigmas, FUN = '/')
-          } else {
-            stop('Dimension of bootstrap sample is not correct for the method.')
-          }
+          sqrtSigma$res = sweep(sqrtSigma$res, 1, rboot(n)/sqrt(1-h), '*')
+          #sigmas = sqrt(colSums(qr.resid(sqrtSigma$QR, sqrtSigma$res)^2)/rdf)
+          #sqrtSigma$res = sweep(sqrtSigma$res, 2, sigmas, FUN = '/')
         } else if (method=='permutation'){
           sqrtSigma$res = sqrtSigma$res[sample(n),]
           #sqrtSigma$res = sqrtSigma$res[1:n,]
